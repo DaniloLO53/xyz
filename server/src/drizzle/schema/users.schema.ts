4676b7248732854672116ecef71a2f93ee5c2c45
DaniloLO53 <daniloleaodev@gmail.com>
2024-12-10 12:54:39 -0300

    created seed.ts

--- server/src/drizzle/schema/users.schema.ts
+++ server/src/drizzle/schema/users.schema.ts
@@ -2,7 +2,6 @@ import { pgTable, serial, text } from 'drizzle-orm/pg-core';
 
 export const users = pgTable('users', {
   id: serial('id').primaryKey(),
-  name: text('name').notNull(),
   email: text('email').notNull(),
   password: text('password').notNull(),
 });
---
+++ server/src/drizzle/seed.ts
@@ -0,0 +1,51 @@
+import { drizzle, NodePgDatabase } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+import * as schema from './schema/schema';
+import 'dotenv/config';
+import { faker } from '@faker-js/faker';
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: false,
+});
+const db = drizzle(pool, { schema }) as NodePgDatabase<typeof schema>;
+
+async function main() {
+  const userIds = await Promise.all(
+    Array(50)
+      .fill('')
+      .map(async () => {
+        const user = await db
+          .insert(schema.users)
+          .values({
+            email: faker.internet.email(),
+            password: '',
+          })
+          .returning();
+
+        return user[0].id;
+      }),
+  );
+
+  await Promise.all(
+    Array(50)
+      .fill('')
+      .map(async () => {
+        const wallet = await db
+          .insert(schema.wallets)
+          .values({
+            user_id: faker.helpers.arrayElement(userIds),
+          })
+          .returning();
+
+        return wallet[0].id;
+      }),
+  );
+}
+
+main()
+  .then()
+  .catch((err) => {
+    console.error(err);
+    process.exit(0);
+  });
